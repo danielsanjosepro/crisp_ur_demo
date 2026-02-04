3364867e2942840a7503f4ee7e9b7c9ea1f6dfc0
Vincenzo <vincenzo.orl98@gmail.com>
2026-02-04 10:55:25 +0100

    added UR30

--- crisp_ur_demos/setup.py
+++ crisp_ur_demos/setup.py
@@ -11,11 +11,40 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
+        # launch
         (os.path.join("share", package_name, "launch"), glob("launch/*.py")),
+        # top-level config files
         (os.path.join("share", package_name, "config"), glob("config/*.yaml")),
         (os.path.join("share", package_name, "config"), glob("config/*.xacro")),
         (os.path.join("share", package_name, "config"), glob("config/*.xml")),
-        (os.path.join("share", package_name, "config/assets"), glob("config/assets/*")),
+        # ---- MuJoCo config (new structure) ----
+        # scene / selector
+        (
+            os.path.join("share", package_name, "config/mujoco"),
+            glob("config/mujoco/*.xml"),
+        ),
+        # UR5e model + assets
+        (
+            os.path.join("share", package_name, "config/mujoco/ur5e"),
+            glob("config/mujoco/ur5e/*.xml"),
+        ),
+        (
+            os.path.join("share", package_name, "config/mujoco/ur5e/assets"),
+            glob("config/mujoco/ur5e/assets/*"),
+        ),
+        # UR30 model + meshes
+        (
+            os.path.join("share", package_name, "config/mujoco/ur30"),
+            glob("config/mujoco/ur30/*.xml"),
+        ),
+        (
+            os.path.join("share", package_name, "config/mujoco/ur30/meshes/visual"),
+            glob("config/mujoco/ur30/meshes/visual/*"),
+        ),
+        (
+            os.path.join("share", package_name, "config/mujoco/ur30/meshes/collision"),
+            glob("config/mujoco/ur30/meshes/collision/*"),
+        ),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
